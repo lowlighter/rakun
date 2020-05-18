1682c11cfb5a6349f7dde5aa9e255fd2f6be8366
lowlighter <simon.lecoq@live.fr>
2020-05-18 09:35:49 +0200

    Update threshold.js

--- tests/threshold.js
+++ tests/threshold.js
@@ -8,36 +8,45 @@
     default:{threshold:"90%"}
   })
 
-//Parse results
-  const results = JSON.parse(fs.readFileSync(path.join(__dirname, "results.json")))
-  console.log(``)
-
-//Compute threshold and success rate
-  const threshold = Number(argv.threshold.replace("%", ""))||0
-  const success = 100*results.numPassedTests/results.numTotalTests
-
-//Update result
-  if (process.env.BADGES_TOKEN) {
-    axios.post("https://badges.lecoq.io/memory", {
-      token:process.env.BADGES_TOKEN,
-      rakun_tests_passed:results.numPassedTests,
-      rakun_tests_total:results.numTotalTests,
-      rakun_tests_accuracy:`${success.toFixed(1).padStart(3)}%`,
-    })
-    console.log(`Sent results to https://badges.lecoq.io/memory`)
+//Process
+;(async function () {
+  //Parse results
+    const results = JSON.parse(fs.readFileSync(path.join(__dirname, "results.json")))
     console.log(``)
-  }
 
-//Verdict
-  console.log(`Success rate : ${success.toFixed(1).padStart(3)}%`)
-  console.log(`Threshold    : ${threshold.toFixed(1).padStart(3)}%`)
-  console.log(``)
-  if (success >= threshold) {
-    console.log(`Result       : pass`.green)
-    process.exit(0)
-  }
-  else {
-    console.log(`Result       : failed`.red)
-    process.exit(1)
-  }
-  
+  //Compute threshold and success rate
+    const threshold = Number(argv.threshold.replace("%", ""))||0
+    const success = 100*results.numPassedTests/results.numTotalTests
+
+  //Update result
+    if (process.env.BADGES_TOKEN) {
+      try {
+        await axios.post("https://badges.lecoq.io/memory", {
+          token:process.env.BADGES_TOKEN,
+          rakun_tests_passed:results.numPassedTests,
+          rakun_tests_total:results.numTotalTests,
+          rakun_tests_accuracy:`${success.toFixed(1).padStart(3)}%`,
+        })
+        console.log(`Sent results to https://badges.lecoq.io/memory`)
+        console.log(``)
+      } 
+      catch (error) {
+        console.log(`Error while sending results to https://badges.lecoq.io/memory`.red)
+        console.log(`${error.response}`.red)
+        console.log(``)
+      }
+    }
+
+  //Verdict
+    console.log(`Success rate : ${success.toFixed(1).padStart(3)}%`)
+    console.log(`Threshold    : ${threshold.toFixed(1).padStart(3)}%`)
+    console.log(``)
+    if (success >= threshold) {
+      console.log(`Result       : pass`.green)
+      process.exit(0)
+    }
+    else {
+      console.log(`Result       : failed`.red)
+      process.exit(1)
+    }
+})
